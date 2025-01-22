a3670d59340afd633af430214475c996666df5ca
manshu <manshu@git.com>
2025-01-22 13:57:43 +0530

    Added announce request and response function

--- tracker.js
+++ tracker.js
@@ -3,6 +3,8 @@ import { Buffer } from 'buffer';
 import { URL } from 'url';
 import crypto from 'crypto';
 
+
+import { genId } from './util';
 // 1. Send a connect request
 // 2. Get the connect response and extract the connection id
 // 3. Use the connection id to send an announce request - this is where we tell the tracker which files we’re interested in
@@ -50,10 +52,73 @@ function respType(resp) {
     }
   }
   
-  function buildAnnounceReq(connId) {
-    // ...
+  // port = between 6881 and 6889
+  function buildAnnounceReq(connId, torrent, port=6881) {
+    let buf = Buffer.allocUnsafe(98);
+
+    // coonnection_id = 8-bytes, 0 
+    connId.copy(buf, 0);
+
+    // action = 1, 4-bytes. 8
+    buf.writeUint32BE(1, 8);
+
+    // trasaction_id = 4-bytes, 12
+    crypto.randomBytes(4).copy(buf, 12);
+
+    // info hash = 20-byte, 16
+    torrentParser.infoHash(torrent).copy(buf, 16);
+
+    // peer id = 20-byte, 36 
+    genId.copy(buf, 36);
+
+    // downloaded = 8-bytes, 56
+    Buffer.alloc(8).copy(buf, 56);
+
+    // left 8-bytes, 64
+    torrentParser.size(torrent).copy(buf, 64);
+
+    // uplaoded 8-bytes, 72
+    Buffer.alloc(8).copy(buf, 72);
+
+    // event 4-bytes 80
+    buf.writeUint32BE(0, 80);
+
+    // ip adress=0, 4-bytes, 84
+    buf.writeUint32BE(0, 84);
+
+    // key=random, 4-bytes, 88
+    crypto.randomBytes(4).copy(buf, 88);
+
+    // num_want = -1, 4-bytes, 92
+    buf.writeInt32BE(-1, 92);
+
+    // port 2-bytes, 96
+    buf.writeUInt16BE(port, 96);
+
+    return buf;
   }
   
   function parseAnnounceResp(resp) {
-    // ...
+
+    function group(interable, groupSize) {
+        let groups = []
+        for (let i = 0; i < interable.length; i += groupSize) {
+            ips.push(interable.slice(i, i + groupSize));
+        }
+        return groups;
+    }
+
+    return {
+        action : resp.readUint32BE(0), // 4-bytes, 0
+        trasactionId : resp.readUint32BE(4), // 4- bytes, 4
+        interval : resp.readUint32BE(8), // 4-bytes, 8
+        leechers : resp.readUint32BE(12), // 4-bytes, 12 
+        seeders : resp.readUint32BE(16), // 4-bytes, 16
+        peers : group(resp.slice(20), 6).map(address => {
+            return {
+                ip : address.slice(0, 4).join('.'),
+                port : address.readUint16BE(4)
+            }
+        })
+    }
   }
